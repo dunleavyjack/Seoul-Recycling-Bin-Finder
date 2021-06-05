f9e954a3ce0d6821baf916c99260e82928577366
dunleavyjack <dunleavyjack@gmail.com>
2021-06-06 00:43:22 +0900

    Trying

--- src/utils/helperFunctions.js
+++ src/utils/helperFunctions.js
@@ -1,5 +1,4 @@
 export const getGridPosition = (address) => {
-    // for href url kakao api takes
     const addressStr = address.toCoords().toString();
     const xCoor = addressStr.split(',')[0].substring(1);
     const yCoor = addressStr.split(',')[1].slice(0, -1);
