8c3a90ccc8f22a07c2561c1697d722a38055e9f1
dunleavyjack <dunleavyjack@gmail.com>
2021-08-08 18:08:50 +0900

    Added pages

---
+++ src/pages/DemoPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DemoMap from '../components/map/DemoMap';
+
+const DemoPage = () => {
+    return (
+        <div className="kakaomap">
+            <DemoMap />
+        </div>
+    );
+};
+
+export default DemoPage;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SplashHeader from '../components/SplashHeader';
+import Overview from '../components/Overview';
+
+const HomePage = () => {
+    return (
+        <>
+            <SplashHeader />
+            <Overview />
+        </>
+    );
+};
+
+export default HomePage;
